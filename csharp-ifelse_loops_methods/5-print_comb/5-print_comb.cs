fc2c4303739e956325fa85fc36517818c90366da
AdrianWolfP <wolfy52032@gmail.com>
2024-05-07 18:02:15 -0500

    worked in file 5

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int display = 0;
+            for (; display < 99; ++display);
+            {
+                Console.Write("{0:D2}, ", display);
+            }
+            Console.Write("{0:D2}\n", display);
+        }
+    }
+}
