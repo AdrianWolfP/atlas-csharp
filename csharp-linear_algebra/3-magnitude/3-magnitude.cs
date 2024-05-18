031c79c5dbaa520ffaef5dc976c8c19716693102
AdrianWolfP <wolfy52032@gmail.com>
2024-05-18 10:19:17 -0500

    added new dir and files

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,27 @@
+using System;
+
+public static class VectorMath
+{
+    /// <summary>
+    /// This method calculates the magnitude (length) of a double array (vector) representing a 2D or 3D vector.
+    /// The method checks if the length of the input vector is within the valid range (2-3).
+    /// If the input vector does not meet this condition, the method returns -1 to indicate an error.
+    /// </summary>
+    /// <param name="vector">The double array representing a 2D or 3D vector.</param>
+    /// <returns>The magnitude (length) of the input vector, or -1 to indicate an error.</returns>
+    /// <exception cref="ArgumentException">Thrown when the length of the input vector is outside the valid range (2-3).</exception>
+    public static double Magnitude(double[] vector)
+    {
+        // Check if the length of the input vector is within the valid range (2-3).
+        if ( vector.Length > 3 || vector.Length < 2)
+            // Return -1 to indicate an error.
+            return -1;
+        
+        // Calculate the sum of the squares of the vector's elements.
+        double sum = 0;
+        foreach (double i in vector)
+            sum += i * i;
+        // Return the magnitude (length) of the input vector.
+        return Math.Round(Math.Sqrt(sum), 2);
+    }
+}
